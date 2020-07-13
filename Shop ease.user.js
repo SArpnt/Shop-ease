6be59cdbfaaad537b10f1068c48bb4ed530149e6
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-07-12 20:11:19 -0400

    domain change update

--- Shop ease.user.js
+++ Shop ease.user.js
@@ -1,15 +1,20 @@
 // ==UserScript==
 // @name         Shop ease
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      2.0.0
 // @run-at       document-start
 // @description  Makes shop opening and closing look nicer
 // @author       SArpnt
-// @match        https://play.boxcritters.com/*
+// @match        https://boxcritters.com/play/
+// @match        https://boxcritters.com/play/?*
+// @match        https://boxcritters.com/play/#*
+// @match        https://boxcritters.com/play/index.html
+// @match        https://boxcritters.com/play/index.html?*
+// @match        https://boxcritters.com/play/index.html#*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
-// @require      https://github.com/sarpnt/joinFunction/raw/master/script.js
-// @require      https://github.com/sarpnt/EventHandler/raw/master/script.js
+// @require      https://github.com/SArpnt/joinFunction/raw/master/script.js
+// @require      https://github.com/SArpnt/EventHandler/raw/master/script.js
 // @require      https://github.com/SArpnt/cardboard/raw/master/script.user.js
 // ==/UserScript==
 
@@ -18,17 +23,17 @@
 
 	cardboard.on('loadScriptClient', function (t) {
 		t.innerHTML = t.innerHTML.replace(
-			/e.x\s*=\s*this.width\s*-\s*e.width\s*\*\s*o,/,
-			`e.x = this.width,
-			createjs.Tween.get(e, { override: true })
-				.to({ x: this.width - e.width * o }, 300, createjs.Ease.cubicOut),`
+			/shop\.x\s*=\s*this\.width\s*-\s*shop\.width\s*\*\s*o/,
+			`shop.x = this.width,
+			createjs.Tween.get(shop, { override: true })
+				.to({ x: this.width - shop.width * o }, 300, createjs.Ease.cubicOut)`
 		).replace(
-			/y\.on\s*\(\s*["'`]click["'`]\s*,\s*\(\s*function\s*\(\)\s*{\s*this\.parent\.removeChild\s*\(\s*this\s*\)\s*[,;]?\s*}\s*\)\s*,\s*this\s*\)/,
-			`y.on("click", function () {
+			/d\.on\s*\(\s*["'`]click["'`]\s*,\s*\(?\s*function\s*\(\)\s*\{\s*this\.parent\.removeChild\s*\(\s*this\s*\)\s*[,;]?\s*\}\s*\)?\s*,\s*this\s*\)/,
+			`d.on("click", function () {
 				createjs.Tween.get(this, { override: true })
 					.to({ x: this.stage.width }, 300, createjs.Ease.cubicIn)
 					.call(function () { this.parent.removeChild(this); });
-			}, this);`
+			}, this)`
 		);
 	});
 })();
